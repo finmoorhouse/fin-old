99c5833e86ec944cb562ac4dda9cf29a532e6613
Fin Moorhouse <5040505+finmoorhouse@users.noreply.github.com>
2020-06-17 18:21:37 +0100

    Added CV and new portfolio page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,27 +13,26 @@ const IndexPage = () => (
       <h1 class="page-title">About me</h1>
       <hr />
       <p>
-        I'm a student based between Bath and Cambridge. I make films and design
+        I'm a student based between Bath and Cambridge. I code, make films, and design
         things.
       </p>
       <p>
         I have experience with short films, event coverage, motion graphics,
         branding, and web design. I'm currently working on a{" "}
-        <Link to="/portfolio/packhorse">documentary</Link> about a village pub.
+        <Link to="/portfolio/packhorse">documentary</Link> about a village pub and interning with <a href='https://www.charityentrepreneurship.com/' target='_none'>these folks</a>. Read my CV <a href='https://www.finmoorhouse.com/Fin-Moorhouse-CV.pdf' target='_none'>here</a>.
       </p>
       <p>
-        I co-host a podcast called <i>Hear This Idea</i> which showcases new thinking from top academics at the University of Cambridge and beyond. You can find it at <a href='https://www.hearthisidea.com' target='_none'>hearthisidea.com</a>.
+        I co-host a podcast called <i>Hear This Idea</i> which showcases new thinking from top academics in philosophy and the social sciences. You can find it at <a href='https://www.hearthisidea.com' target='_none'>hearthisidea.com</a>.
       </p>
       <p>
         Lastly, I'm interested in{" "}
         <a href="https://www.effectivealtruism.org/" target="blank">
           effective altruism
-        </a>{" "}
-        and related ideas like long-termism and existential risk. I'm hoping to
+        </a> and related ideas like longtermism and existential risk. I'm hoping to
         write about this kind of thing on my 'writing' page and elsewhere.
       </p>
       <p>
-        In my free time, I like to row and{" "}
+        In my free time, I like to row, cycle, and{" "}
         <a href="http://www.goodreads.com/finm" target="blank">
           read.
         </a>
