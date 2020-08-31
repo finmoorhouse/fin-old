34d0ada14671952d0e5b7d141acfa34901901928
Fin Moorhouse <5040505+finmoorhouse@users.noreply.github.com>
2020-08-31 16:54:25 +0100

    Added h-entry to blog posts.

--- src/components/post.js
+++ src/components/post.js
@@ -4,7 +4,7 @@ import { MDXRenderer } from "gatsby-plugin-mdx"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
 import "../styles/post.scss"
-import "katex/dist/katex.min.css";
+import "katex/dist/katex.min.css"
 import SEO from "../components/seo"
 
 export const query = graphql`
@@ -28,25 +28,32 @@ export const query = graphql`
   }
 `
 
-const ThemeContext = React.createContext();
+const ThemeContext = React.createContext()
 
 const PostTemplate = function({ data: { mdx: post } }) {
   let backTo = "/writing"
   if (post.frontmatter.type === "portfolio") {
     backTo = "/portfolio"
   }
-  const [dark,setDark] = useState(false);
-  
+  const [dark, setDark] = useState(false)
+
   return (
     <Layout theme={dark}>
-      <SEO title={post.frontmatter.title}  myFeaturedImage={post.frontmatter.featuredImage} />
+      <SEO
+        title={post.frontmatter.title}
+        myFeaturedImage={post.frontmatter.featuredImage}
+      />
       <div className="page-wrapper post h-entry">
-  <button className = "dark-mode-toggle" onClick = {() => setDark(! dark)}>
-  <svg viewBox="0 0 600 600">
-    <path d="M283.211 512c78.962 0 151.079-35.925 198.857-94.792 7.068-8.708-.639-21.43-11.562-19.35-124.203 23.654-238.262-71.576-238.262-196.954 0-72.222 38.662-138.635 101.498-174.394 9.686-5.512 7.25-20.197-3.756-22.23A258.156 258.156 0 0 0 283.211 0c-141.309 0-256 114.511-256 256 0 141.309 114.511 256 256 256z" id="moon"/>
-    </svg>
-  </button>
+        <button className="dark-mode-toggle" onClick={() => setDark(!dark)}>
+          <svg viewBox="0 0 600 600">
+            <path
+              d="M283.211 512c78.962 0 151.079-35.925 198.857-94.792 7.068-8.708-.639-21.43-11.562-19.35-124.203 23.654-238.262-71.576-238.262-196.954 0-72.222 38.662-138.635 101.498-174.394 9.686-5.512 7.25-20.197-3.756-22.23A258.156 258.156 0 0 0 283.211 0c-141.309 0-256 114.511-256 256 0 141.309 114.511 256 256 256z"
+              id="moon"
+            />
+          </svg>
+        </button>
         <h1 className="page-title">{post.frontmatter.title}</h1>
+  <a rel='Author' className='p-author h-card hidden' href="https://www.finmoorhouse.com">{post.frontmatter.author}</a>
 
         <hr />
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,41 +4,66 @@ import Layout from "../components/layout"
 
 import SEO from "../components/seo"
 
-
 const IndexPage = () => (
   <Layout path="index-colour">
-   
-    <SEO title="👋 About Me" pageThumb = "/meta-about.jpg"/>
-    
-    <div class="page-wrapper index-wrapper big">
+    <SEO title="👋 About Me" pageThumb="/meta-about.jpg" />
+
+
+
+
+
+    <div class="page-wrapper index-wrapper big h-card">
+    <a class="p-name u-url hidden" href="https://finmoorhouse.com/">
+        Fin Moorhouse
+      </a>
       <h1 class="page-title">About me</h1>
       <hr />
       <p>
-        I'm a student based between Bath and Cambridge. I code, make films, and design
-        things.
+        I'm a <span class='p-job-title'>student</span> based between Bath and Cambridge. I code, make films, and
+        design things.
       </p>
       <p>
         I have experience with short films, event coverage, motion graphics,
         branding, and web design. I'm currently working on a{" "}
-        <Link to="/portfolio/packhorse">documentary</Link> and interning with <a href='https://www.charityentrepreneurship.com/' target='_none'>these folks</a>. Read my CV <a href='https://www.finmoorhouse.com/Fin-Moorhouse-CV.pdf' target='_none'>here</a>.
+        <Link to="/portfolio/packhorse">documentary</Link> and interning with{" "}
+        <a href="https://www.charityentrepreneurship.com/" target="_none">
+          these folks
+        </a>
+        . Read my CV{" "}
+        <a
+          href="https://www.finmoorhouse.com/Fin-Moorhouse-CV.pdf"
+          target="_none"
+        >
+          here
+        </a>
+        .
       </p>
       <p>
-        I co-host a podcast called <i>Hear This Idea</i> which showcases new thinking from academics and innovators in philosophy and the social sciences. You can find it at <a href='https://www.hearthisidea.com' target='_none'>hearthisidea.com</a>.
+        I co-host a podcast called <i>Hear This Idea</i> which showcases new
+        thinking from academics and innovators in philosophy and the social
+        sciences. You can find it at{" "}
+        <a href="https://www.hearthisidea.com" target="_none">
+          hearthisidea.com
+        </a>
+        .
       </p>
       <p>
         Lastly, I'm interested in{" "}
         <a href="https://www.effectivealtruism.org/" target="blank">
           effective altruism
-        </a> and related ideas like longtermism and existential risk. I'm hoping to
-        write about this kind of thing on my <Link to="/writing">writing</Link> page and elsewhere.
+        </a>{" "}
+        and related ideas like longtermism and existential risk. I'm hoping to
+        write about this kind of thing on my <Link to="/writing">writing</Link>{" "}
+        page and elsewhere.
       </p>
       <p>
-        I <a href="http://www.goodreads.com/finm" target="blank">
+        I{" "}
+        <a href="http://www.goodreads.com/finm" target="blank">
           read
-        </a> in my free time.
+        </a>{" "}
+        in my free time.
       </p>
     </div>
-   
   </Layout>
 )
 
