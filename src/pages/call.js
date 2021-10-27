6bf18dcf16800410a07e985552730c8905f6b48a
finmoorhouse <5040505+finmoorhouse@users.noreply.github.com>
2021-10-27 10:02:25 +0100

    Got page redirect working.

---
+++ src/pages/call.js
@@ -0,0 +1,29 @@
+import React from "react"
+import Layout from "../components/layout"
+
+import SEO from "../components/seo"
+
+class CallPage extends React.Component {
+  componentDidMount() {
+    window.location.replace("https://calendly.com/finmoorhouse/call")
+  }
+  render() {
+    return (
+      <>
+        <Layout>
+          <SEO title="📞 Call" pageThumb="/meta-image.jpg" />
+          <h1 className="page-title">Call</h1>
+          <hr />
+          <p>
+            You should be redirected to a place where you can book a call with
+            me. If that doesn't work,{" "}
+            <a href="https://calendly.com/finmoorhouse/call">click this link</a>
+            .
+          </p>
+        </Layout>
+      </>
+    )
+  }
+}
+
+export default CallPage
