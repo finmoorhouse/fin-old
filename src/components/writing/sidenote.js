9d0c4da3aa855d9720e523c59ac2e5ab3cffa5a0
finmoorhouse <5040505+finmoorhouse@users.noreply.github.com>
2021-12-27 22:32:35 +0000

    Disabled error message.

--- src/components/writing/sidenote.js
+++ src/components/writing/sidenote.js
@@ -3,6 +3,7 @@ import React from "react"
 function Sidenote(props) {
   return (
     <>
+      <>{ /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }</>
       <label for={props.label} htmlFor={props.label} className="margin-toggle sidenote-number"></label>
       <input type="checkbox" id={props.label} className="margin-toggle"/>
       <span className="sidenote">{props.children}</span>
