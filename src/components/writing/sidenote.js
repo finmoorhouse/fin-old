822b2a1918d5a81072cb206813b2cdbbdf7db0ca
finmoorhouse <5040505+finmoorhouse@users.noreply.github.com>
2021-12-27 22:36:37 +0000

    Added backup label.

--- src/components/writing/sidenote.js
+++ src/components/writing/sidenote.js
@@ -1,11 +1,12 @@
 import React from "react"
 
 function Sidenote(props) {
+  const forLabel = props.label || Math.random().toString(36).substr(2, 9)
   return (
     <>
       <>{ /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }</>
-      <label for={props.label} htmlFor={props.label} className="margin-toggle sidenote-number"></label>
-      <input type="checkbox" id={props.label} className="margin-toggle"/>
+      <label for={forLabel} htmlFor={forLabel} className="margin-toggle sidenote-number"></label>
+      <input type="checkbox" id={forLabel} className="margin-toggle"/>
       <span className="sidenote">{props.children}</span>
     </>
   )
